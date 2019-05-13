2e5ebef58482f8c31d3443ad3af16d9c0331dc66
ADIL NAIMI <adilnm23@hotmail.com>
2019-05-13 09:13:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,12 +24,12 @@ class Application
           resp.write "#{item}\n"
       end
     end
-  elsif req.path.match(/add/)
-    search_term = req.params["item"]
-    if @@items.include?(search_term)
-      @@cart << search_term
-      resp.write "Added #{search_term}"
-    end
+  # elsif req.path.match(/add/)
+  #   search_term = req.params["item"]
+  #   if @@items.include?(search_term)
+  #     @@cart << search_term
+  #     resp.write "Added #{search_term}"
+  #   end
   else
       resp.write "Path Not Found"
     end
