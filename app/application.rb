d590fec1444564d675f949e4a199c3f7516dca09
ADIL NAIMI <adilnm23@hotmail.com>
2019-05-13 08:37:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,9 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif @@items.empty?
+      resp.write "Your cart is empty"
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
