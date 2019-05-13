7ea63c7e326ae4db54287a2b5b97a564cef7f430
ADIL NAIMI <adilnm23@hotmail.com>
2019-05-13 08:39:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,8 +14,8 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif @@items.empty?
-      resp.write "Your cart is empty"
+    # elsif @@items.empty?
+    #   resp.write "Your cart is empty"
     else
       resp.write "Path Not Found"
 
